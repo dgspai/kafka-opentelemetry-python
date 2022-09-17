bfb0bd47ec829b5cfb9b5303d4252906bb5d2da9
dgspai@hotmail.com <dgspai@hotmail.com>
2022-09-17 13:20:09 -0300

    feat: Add basic kafka implementation

---
+++ producer.py
@@ -0,0 +1,7 @@
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+
+# Asynchronous by default
+future = producer.send('my-topic', b'raw_bytes')
